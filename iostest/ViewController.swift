e29fd2a14ae0b14775d866449db8a58523f0fae6
ciec-20171103189 <1103385493@qq.com>
2018-10-13 09:52:18 +0800

    test
    相加

--- iostest/ViewController.swift
+++ iostest/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var clmX: UITextField!
+    
+    @IBOutlet weak var clmY: UITextField!
+    
+    @IBOutlet weak var clmZ: UITextField!
+    
+    @IBAction func caculator(_ sender: Any) {
+        clmZ.text = "\(Int(clmX.text!)! + Int(clmY.text!)!)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        clmX.text="0"
+        clmY.text="0"
+        clmZ.text="0"
     }
 
 
